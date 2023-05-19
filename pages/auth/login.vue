9c99e1b86bf55ca202bf3a9a8b4c3b91e1c52b25
Spacesick <vincent.yovian01@ui.ac.id>
2023-05-19 21:01:51 +0700

    Update login and register pages

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -16,28 +16,35 @@
         Login
       </button>
       <div class="flex justify-center text-sm mt-3">
-        <span>Belum punya akun? <router-link :to="{ path: '/auth/register'}" class="text-blue-600 hover:text-blue-700">Daftar</router-link></span>
+        <span
+          >Belum punya akun?
+          <router-link
+            :to="{ path: '/auth/register' }"
+            class="text-blue-600 hover:text-blue-700"
+            >Daftar</router-link
+          ></span
+        >
       </div>
     </form>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-import { parseJwt } from '~/store/auth'
-import CInput from '~/components/auth/CInput.vue'
+import { mapActions } from 'vuex';
+import { parseJwt } from '~/store/auth';
+import CInput from '~/components/auth/CInput.vue';
 
 export default {
   components: {
     CInput,
   },
-  beforeRouteEnter (to, from, next) {
+  beforeRouteEnter(to, from, next) {
     if (localStorage.getItem('lawToken')) {
       const jwtPayload = parseJwt(localStorage.getItem('lawToken'));
-      if (jwtPayload.exp < Date.now()/1000) {
+      if (jwtPayload.exp < Date.now() / 1000) {
         // token expired
         localStorage.removeItem('lawToken');
-        next()
+        next();
       }
       next('/');
     } else {
@@ -50,22 +57,22 @@ export default {
       email: '',
       password: '',
       inputData: [
-      {
+        {
           label: 'Username',
           type: 'text',
           placeholder: 'mrhacka',
           val: '',
-          required: true
+          required: true,
         },
         {
           label: 'Password',
           type: 'password',
           placeholder: '********',
           val: '',
-          required: true
+          required: true,
         },
       ],
-    }
+    };
   },
   methods: {
     ...mapActions({
@@ -73,20 +80,17 @@ export default {
     }),
     submitForm: async function () {
       if (!this.$refs.form.checkValidity()) {
-        // remove alert on prod
-        alert('gagal')
-        return
+        return;
       }
 
-      const res = await this.$axios.$post('http://34.28.48.143/api/token/', {
+      const res = await this.$axios.$post('http://34.28.48.143/auth/token/', {
         username: this.username,
-        password: this.password
-      })
+        password: this.password,
+      });
 
-      this.setToken(res.access)
-      this.$router.push('/')
-    }
+      this.setToken(res.access);
+      this.$router.push('/');
+    },
   },
-
-}
+};
 </script>
