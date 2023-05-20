fa0cf828316d6e40b08f22e7bacdca3ca2435ad7
Spacesick <vincent.yovian01@ui.ac.id>
2023-05-20 14:19:10 +0700

    Add user preview template

--- components/chat/UserPreview.vue
+++ components/chat/UserPreview.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="w-full text-sm font-medium">
+    <button
+      class="w-full flex justify-between px-2 py-2 mb-1 rounded-md border-none outline-none hover:bg-slate-200 transition-all focus:border-none focus:outline-none"
+      :class="[selected ? 'bg-slate-200' : '']"
+      @click="$emit('select')"
+    >
+      <div>
+        {{ user.username }}
+      </div>
+      <div
+        v-if="user.unreadMessages"
+        class="self-center rounded-md bg-red-600 text-white text-xs px-1.5"
+      >
+        {{ user.unreadMessages }}
+      </div>
+    </button>
   </div>
 </template>
 
