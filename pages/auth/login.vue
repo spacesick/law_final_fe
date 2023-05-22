ce91eed70302e22cd16d7f33adf87ebb3c8e3193
Spacesick <vincent.yovian01@ui.ac.id>
2023-05-22 21:46:33 +0700

    Update header, login page, and register page styling

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -10,7 +10,7 @@
         <CInput v-model="password" v-bind="inputData[1]" />
       </div>
       <button
-        class="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center"
+        class="mt-4 text-white bg-blue-700 border-blue-800 hover:bg-blue-800 transition-all focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center"
         @click.prevent="submitForm"
       >
         Login
